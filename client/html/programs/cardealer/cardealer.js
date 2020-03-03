d157a119855b7b0344a61a109186c54e5680fe4d
jonas <jonassvensson4@hotmail.com>
2020-03-03 12:28:26 +0100

    Cardealer program updated
    I've now added a vehicle api that the program fetches information from. You'll now be able to see some data about the vehicle, stats, model name, default ingame price and some more. Have a look for yourself!

--- client/html/programs/cardealer/cardealer.js
+++ client/html/programs/cardealer/cardealer.js
@@ -1,31 +1,18 @@
-let vehicles;
-let blacklistedCategory = ['military', 'industrial', 'emergency', 'utility', 'helicopters', 'planes', 'service', 'boats', 'commercial'];
+let blacklistedCategory = ['military', 'industrial', 'emergency', 'utility', 'helicopters', 'planes', 'service', 'boats', 'commercial', 'trains'];
 let blacklistedVehicle = ['caracara', 'insurgent', 'insurgent pick-up', 'insurgent pick-ip custom', 'menacer', 'ramp buggy', 'technical', 'technical aqua', 'technical custom', 'oppressor', 'oppressor mk ii', 'vigilante (car)', 'armored boxville', 'turreted limo'];
-let categories = [];
+let showLarge = false;
+let currentAngle;
 
-$.getJSON('programs/cardealer/vehicles.json', function( data )
-{
-    vehicles = data;
-
-    Object.keys( vehicles ).forEach(( k ) => {
-        let category = k;
-
-        if ( !categories.includes( category ) && !blacklistedCategory.includes( category ) ) {
-            categories.push( category );
-
-            let vehicle = Object.keys( vehicles[k] )[0];
-
-            if ( category === 'vans' ) {
-                vehicle = Object.keys( vehicles[k] )[3];
-            }
-
-            let frontQuarter = vehicles[k][vehicle].frontQuarter;
-
+fetch('https://gta.now.sh/api/vehicles/class')
+.then(response => response.json())
+.then(categories => {
+    Object.keys( categories ).forEach(category => {
+        if ( !blacklistedCategory.includes(category) ) {
             let div = `<div class="col s4">
-                <a href="#" class="cardealer-category" onclick="showVehicleCategory('${category}')" category="${ category }">
+                <a href="#?cardealer-category=${ category }" class="cardealer-category" onclick="showVehicleCategory('${category}')">
                     <div class="card">
                         <div class="card-image">
-                            <img src="${ frontQuarter }" draggable="false">
+                            <img src="${ categories[category] }" draggable="false">
                         </div>
                         <div class="card-content">
                             <span class="card-title">${ category }</span>
@@ -39,82 +26,97 @@ $.getJSON('programs/cardealer/vehicles.json', function( data )
     });
 });
 
+function showLargeImage( angle ) {
+    currentAngle = angle;
+    
+    $(`.${ angle }`).removeClass('hidden');
+
+    $('#large-image').fadeIn('fast', function() {
+        showLarge = true;
+    });
+}
+
+function showVehicle( vehicleName ) {
+    $('#large-image').html('');
+
+    fetch(`https://gta.now.sh/api/vehicles/${ vehicleName }`)
+    .then(response => response.json())
+    .then(vehicle => {
+        let divSmallerImages = '';
+        let divLargerImages = '';
+
+        Object.keys( vehicle.images ).forEach(angle => {
+            divSmallerImages += `<img class="vehicle-image" src="${ vehicle.images[angle] }" onclick="showLargeImage('${ angle }')" />`;
+            divLargerImages += `<img class="${ angle } hidden" src="${ vehicle.images[angle].replace('210', '700') }" />`;
+        });
+
+        $('#cardealer-vehicle-content').html('');
+        $('#large-image').html('');
+
+        $('#cardealer-vehicle-content').append( divSmallerImages );
+        $('#large-image').append( divLargerImages );
+
+        $('#vehicle-model').text(vehicle.model);
+        $('#vehicle-manufacturer').text(vehicle.manufacturer);
+        $('#vehicle-seats').text(vehicle.seats);
+        $('#vehicle-price').text(vehicle.price);
+
+        $('#vehicle-speed .stat-amount').text(vehicle.speed);
+        $('#vehicle-speed .stat-bar').css({ width: `${ vehicle.speed }%` });
+        $('#vehicle-acceleration .stat-amount').text(vehicle.acceleration);
+        $('#vehicle-acceleration .stat-bar').css({ width: `${ vehicle.acceleration }%` });
+        $('#vehicle-braking .stat-amount').text(vehicle.braking);
+        $('#vehicle-braking .stat-bar').css({ width: `${ vehicle.braking }%` });
+        $('#vehicle-handling .stat-amount').text(vehicle.handling);
+        $('#vehicle-handling .stat-bar').css({ width: `${ vehicle.handling }%` });
+        
+        if ( vehicle.topSpeed ) {
+            $('#vehicle-topspeed').text(`${ vehicle.topSpeed.mph } mph | ${ vehicle.topSpeed.kmh } kmh`);
+        }
+        
+        // Ugly timeout to give some extra time for the images to load..
+        $('#cardealer-list').fadeOut(500, () => {
+            $('#cardealer-vehicle h6').text( vehicleName );
+            $('#cardealer-vehicle').fadeIn('fast');
+        });
+    });
+}
+
 function showVehicleCategory( category ) {
     $('#cardealer-list-content').html('');
 
-    $('#cardealer-category').fadeOut('fast', () => {
-        Object.keys( vehicles ).forEach(( k ) => {
-            if ( category === k ) {
-                let vehicle = vehicles[k];
-               
-                Object.keys( vehicle ).forEach(( k ) => {
-                    let name = k;
+    fetch(`https://gta.now.sh/api/vehicles/class/${ category }`)
+    .then(response => response.json())
+    .then(vehicles => {
+        $('#cardealer-category').fadeOut('fast', () => {
+            Object.keys( vehicles ).forEach(vehicle => {
+                if ( !blacklistedVehicle.includes( vehicle ) ) {
+                    let div = `<div class="col s4">
+                        <a href="#?cardealer-category=${ category }&cardealer-vehicle=${ vehicle }" class="cardealer-vehicle" onclick="showVehicle('${ vehicle }')">
+                            <div class="card">
+                                <div class="card-image">
+                                    <img src="${ vehicles[vehicle].images.frontQuarter }" draggable="false">
+                                </div>
+                                <div class="card-content">
+                                    <span class="card-title">${ vehicle }</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>`;
 
-                    if ( name.length > 17 ) {
-                        name = name.split(' ')[0];
-                    }
-
-                    if ( !blacklistedVehicle.includes( k ) ) {
-                        if ( vehicle[k].frontQuarter != 'https://vignette.wikia.nocookie.net/gtawiki/images/f/f6/CDG-ImageNeeded.png/revision/latest/scale-to-width-down/210' ) {
-                            let div = `<div class="col s4">
-                                <a href="#" class="cardealer-vehicle" onclick="showVehicle('${ k }', '${ category }')" category="${ category }" name="${ k }">
-                                    <div class="card">
-                                        <div class="card-image">
-                                            <img src="${ vehicle[k].frontQuarter }" draggable="false">
-                                        </div>
-                                        <div class="card-content">
-                                            <span class="card-title">${ name }</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>`;
-
-                            $('#cardealer-list-content').append( div );
-                        }
-                    }
-                });
-            }
+                    $('#cardealer-list-content').append( div );
+                }
+            });
         });
 
         $('#cardealer-list-header h6').text( category );
+        $('#cardealer-list-content').scrollTop();
         $('#cardealer-list').fadeIn('slow');
     });
 }
 
-function showVehicle( name, category ) {
-    div = `
-        <img class="vehicle-resize" angle="frontQuarter" src="${vehicles[category][name].frontQuarter.replace('210', '300')}" />
-        <img class="vehicle-resize" angle="rearQuarter" src="${vehicles[category][name].rearQuarter.replace('210', '300')}" />
-        <img class="vehicle-resize" angle="front" src="${vehicles[category][name].front.replace('210', '200')}" />
-        <img class="vehicle-resize" angle="side" src="${vehicles[category][name].side.replace('210', '200')}" />
-        <img class="vehicle-resize" angle="rear"src="${vehicles[category][name].rear.replace('210', '200')}" />
-    `;
-
-    $('#cardealer-vehicle-content').append( div );
-
-    $('#large-image').html('');
-
-    div = `
-        <img class="frontQuarter hidden" src="${vehicles[category][name].frontQuarter.replace('210', '700')}" />
-        <img class="rearQuarter hidden" src="${vehicles[category][name].rearQuarter.replace('210', '700')}" />
-        <img class="front hidden" src="${vehicles[category][name].front.replace('210', '700')}" />
-        <img class="side hidden" src="${vehicles[category][name].side.replace('210', '700')}" />
-        <img class="rear hidden"src="${vehicles[category][name].rear.replace('210', '700')}" />
-    `;
-
-    $('#large-image').append( div );
-
-    // Ugly timeout to give some extra time for the images to load..
-    $('#cardealer-list').fadeOut(500, () => {
-        $('#cardealer-vehicle h6').text( name );
-        $('#cardealer-vehicle').fadeIn('fast');
-    });
-}
-
-$('body').on('click', '.cardealer-header i', function () {
-    back = $(this).attr('back');
-
-    if ( back === 'category' ) {
+function goBack( type) {
+    if ( type === 'category' ) {
         $('#cardealer-list').fadeOut('fast', () => {
             $('#cardealer-category').fadeIn('fast');
         });
@@ -124,24 +126,11 @@ $('body').on('click', '.cardealer-header i', function () {
             $('#cardealer-list').fadeIn('fast');
         });
     }
-});
+}
 
-let showLarge = false;
-let angle;
-
-$('body').on('click', '.vehicle-resize', function () {
-    angle = $(this).attr('angle');
-
-    $(`.${angle}`).removeClass('hidden');
-
-    $('#large-image').fadeIn('fast', function() {
-        showLarge = true;
-    });
-});
-
-$('body').on('click', '.program-cardealer', function () {
+$('body').on('click', '.program-cardealer', () => {
     if ( showLarge ) {
-        $(`.${angle}`).addClass('hidden');
+        $(`.${ currentAngle }`).addClass('hidden');
         $('#large-image').hide();
         showLarge = false;
     }
